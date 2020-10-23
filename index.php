18a35f75a0b53da6d564437a077bb7d058a04981
windy <71083075+windyoc28@users.noreply.github.com>
2020-10-23 07:35:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>babycaca.id</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+  <title>babycaca.id</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,900|Oswald:300,400,700" rel="stylesheet">
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,900|Oswald:300,400,700" rel="stylesheet">
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/jquery-ui.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/jquery-ui.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
 
-    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+  <link rel="stylesheet" href="css/jquery.fancybox.min.css">
 
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+  <link rel="stylesheet" href="css/bootstrap-datepicker.css">
 
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+  <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
 
-    <link rel="stylesheet" href="css/aos.css">
+  <link rel="stylesheet" href="css/aos.css">
 
-    <link rel="stylesheet" href="css/style.css">
-    
-  </head>
-  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
-  
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
   <div class="site-wrap">
 
     <div class="site-mobile-menu site-navbar-target">
@@ -37,14 +37,14 @@
       </div>
       <div class="site-mobile-menu-body"></div>
     </div>
-   
-<!-- header -->
+
+    <!-- header -->
 
     <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
 
       <div class="container">
         <div class="row align-items-center">
-          
+
           <div class="col-6 col-xl-3">
             <h1 class="mb-0 site-logo m-0 p-0"><a href="index.html" class="mb-0">Babycaca Shop</a></h1>
           </div>
@@ -54,7 +54,7 @@
             <nav class="site-navigation position-relative text-right" role="navigation">
 
               <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
-                <li><a href="?page=marketplace/home/index.php" class="nav-link">Home</a></li>
+                <li><a href="?page=marketplace/marketplace/home/index.php" class="nav-link">Home</a></li>
                 <li><a href="index.html#home-section" class="nav-link">Keranjang</a></li>
                 <li><a href="index.html#home-section" class="nav-link">Masuk</a></li>
               </ul>
@@ -70,47 +70,47 @@
 
     
     <div class="site-block-wrap">
-    <div class="owl-carousel with-dots">
-      <div class="site-blocks-cover overlay overlay-2" style="background-image: url(images/logo.jpg);" data-aos="fade" id="home-section">
+      <div class="owl-carousel with-dots">
+        <div class="site-blocks-cover overlay overlay-2" style="background-image: url(images/logo.jpg);" data-aos="fade" id="home-section">
 
 
-        <div class="container">
-          <div class="row align-items-center justify-content-center">
-            <div class="col-md-6 mt-lg-5 text-center">
-              <h1 class="text-shadow">SEPATU IMPORT</h1>
-              <p class="mb-5 text-shadow">Tampil trendi dengan sepatu kekinian yang membuat semua mata tertuju kepada anda</p>
-              
+          <div class="container">
+            <div class="row align-items-center justify-content-center">
+              <div class="col-md-6 mt-lg-5 text-center">
+                <h1 class="text-shadow">SEPATU IMPORT</h1>
+                <p class="mb-5 text-shadow">Tampil trendi dengan sepatu kekinian yang membuat semua mata tertuju kepada anda</p>
+
+              </div>
             </div>
           </div>
-        </div>
-  
-        
-      </div>  
-  
-       
-    </div>   
-    
-  </div>      
 
 
-   <!-- .site-wrap -->
+        </div>  
 
-  <a href="#top" class="gototop"><span class="icon-angle-double-up"></span></a> 
 
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/jquery-ui.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.countdown.min.js"></script>
-  <script src="js/bootstrap-datepicker.min.js"></script>
-  <script src="js/jquery.easing.1.3.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/jquery.fancybox.min.js"></script>
-  <script src="js/jquery.sticky.js"></script>
+      </div>   
 
-  
-  <script src="js/main.js"></script>
+    </div>      
+
+
+    <!-- .site-wrap -->
+
+    <a href="#top" class="gototop"><span class="icon-angle-double-up"></span></a> 
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/bootstrap-datepicker.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+
+
+    <script src="js/main.js"></script>
     
   </body>
-</html>
+  </html>
